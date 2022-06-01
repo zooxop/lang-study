cdf8b5dd9b4238ad3109985b8378604151536ba2
CheolHyun Mun <zooxop@gmail.com>
2022-06-01 18:52:06 +0900

    spring boot - initial commit of chart project

---
+++ coding/java/ChartProject/src/main/java/com/emr/chartproject/ChartProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emr.chartproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartProjectApplication.class, args);
+    }
+
+}
---
+++ coding/java/ChartProject/src/test/java/com/emr/chartproject/ChartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.chartproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
