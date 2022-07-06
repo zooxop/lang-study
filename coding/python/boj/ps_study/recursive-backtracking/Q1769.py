567d0fc340ccb1bc250b46bc27de65de08cf90cf
CheolHyun Mun <zooxop@gmail.com>
2022-07-06 16:11:02 +0900

    boj-python solved 1769

---
+++ coding/python/boj/ps_study/recursive-backtracking/Q1769.py
@@ -0,0 +1,23 @@
+def solution(value):
+    temp = 0
+    for i in range(0, len(value)):
+        temp += int(value[i])
+
+    return str(temp)
+
+
+X = input()
+cnt = 0
+result = 0
+
+while True:
+    if len(X) < 2:
+        break
+    X = solution(X)
+    cnt += 1
+
+print(cnt)
+if int(X) % 3 == 0:
+    print('YES')
+else:
+    print('NO')
