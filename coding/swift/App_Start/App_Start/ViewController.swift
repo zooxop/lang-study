fa6614f3da591b931e4fd22d4f49bb5a0a100221
CheolHyun Mun <zooxop@gmail.com>
2022-07-13 17:46:41 +0900

    add app_start project and start app project study

---
+++ coding/swift/App_Start/App_Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Start
+//
+//  Created by 문철현 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
