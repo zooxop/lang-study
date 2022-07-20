545110902c123e9691e5fb75d32b273b65388642
CheolHyun Mun <zooxop@gmail.com>
2022-07-20 17:12:54 +0900

    - view controller의 life cycle 정리 - 새로운 view 생성, 클래스와 연결, 버튼 클릭 이벤트로 화면 호출

---
+++ coding/swift/App_Start/App_Start/DetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  DetailVC.swift
+//  App_Start
+//
+//  Created by 문철현 on 2022/07/20.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    // view controller(화면) life cycle
+    
+    // 화면에 올릴 준비가 됐을 때
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // 화면이 나타나기 직전
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // 화면이 나타난 직후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // 화면이 사라지기 직전
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // 화면이 사라진 직후
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+}
--- coding/swift/App_Start/App_Start/ViewController.swift
+++ coding/swift/App_Start/App_Start/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UIViewController {
     @IBAction func doSomething(_ sender: Any) {
         // 받는 타입(backgroundColor)의 타입이 UIColor이기 때문에, UIColor.orange를 .orange로 생략 가능하다.
         testButton.backgroundColor = .orange
+        
+        // 스토리보드 인스턴스화. name에 스토리보드 "파일 이름"을 넣어주면 된다.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // "Main" storyboard 에서 "DetailVC" 라는 이름의 viewController를 불러와서 인스턴스화 한다.
+        // 반드시 storyboard의 "storyboard ID" 속성 값이 설정되어 있어야 한다.
+        let detailVC = storyboard.instantiateViewController(identifier: "DetailVC") as DetailVC
+        
+        // 화면 전환 이벤트
+        self.present(detailVC, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -23,7 +33,4 @@ class ViewController: UIViewController {
         
         testButton.backgroundColor = UIColor.red
     }
-
-
 }
-
