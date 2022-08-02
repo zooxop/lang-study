b5bea24ba678ab1c51ca09e99a11de916b060f97
CheolHyun Mun <zooxop@gmail.com>
2022-08-02 23:31:23 +0900

    programmers-python performance improvement of make_prime_number

--- coding/python/programmers/level1/make_prime_number_comb.py
+++ coding/python/programmers/level1/make_prime_number_comb.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from math import sqrt
 
 
 def is_prime(n):
@@ -8,12 +9,23 @@ def is_prime(n):
     return True
 
 
+def is_prime_sqrt(n):
+    if n <= 2:
+        return True if n == 2 else False
+
+    for i in range(2, int(sqrt(n))+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
 def solution(nums):
     answer = 0
     comb = list(combinations(nums, 3))
 
     for temp in comb:
-        if is_prime(sum(temp)):
+        if is_prime_sqrt(sum(temp)):
             answer += 1
 
     return answer
