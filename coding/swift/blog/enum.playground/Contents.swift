c01ba1dcc732d7e53aff3b5750512bdfc08f36e1
CheolHyun Mun <zooxop@gmail.com>
2022-08-09 01:58:40 +0900

    swift-blog post finished about enum

--- coding/swift/blog/enum.playground/Contents.swift
+++ coding/swift/blog/enum.playground/Contents.swift
@@ -4,7 +4,6 @@ enum Media {
   case book(title: String, author: String, year: Int)
   case movie(title: String, director: String, year: Int)
   case website(urlString: String)
-  case test1
 }
 
 let media = Media.movie(title: "Captain America: Civil War", director: "Russo Brothers", year: 2016)
@@ -14,6 +13,10 @@ case let Media.movie(title, _, _):
     print(title)
 default:
     break
+}
+
+if case let Media.book(title, author, year) = media {
+    print(title, author, year)
 }
 
 
@@ -50,3 +53,45 @@ if day == Weekday.fri {
 } else {
     print("불금이 아님 ㅜ")
 }
+
+
+
+enum Fruit: Int {
+    case apple = 0
+    case grape = 1
+    case peach = 2
+}
+
+print(Fruit.apple)
+// 출력: apple
+print(Fruit.apple.rawValue)
+// 출력: 0
+
+var gra: Fruit = Fruit.grape
+
+if gra == Fruit.grape {
+    print(true)
+}
+
+enum School: String {
+    case elementary = "초등"
+    case middle = "중등"
+    case high = "고등"
+    case university  // 초기화를 안해준다면, case의 이름을 그대로 rawValue로 갖는다.
+}
+
+
+print(School.middle.rawValue)
+// 출력: 중등
+
+print(School.university.rawValue)
+// 출력: university
+
+
+let apple: Fruit? = Fruit(rawValue: 0)
+
+if let orange: Fruit = Fruit(rawValue: 5) {
+    print("rawValue 5에 해당하는 케이스는 \(orange)입니다.")
+} else {
+    print("rawValue 5에 해당하는 케이스가 없습니다.")
+}  //
