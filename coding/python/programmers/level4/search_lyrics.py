53ba3ab48550e1a03556dfc992054c3ea7eab249
CheolHyun Mun <zooxop@gmail.com>
2022-08-13 20:27:29 +0900

    programmers-python search_lyrics solved

--- coding/python/programmers/level4/search_lyrics.py
+++ coding/python/programmers/level4/search_lyrics.py
@@ -1,58 +1,58 @@
-def isMatch(query, word):
-    for i in range(0, len(word)):
-        if query[i] == '?':
-            continue
-        if word[i] != query[i]:
-            return False
-
-    return True
+import sys
+sys.setrecursionlimit(100001)
 
 
 def solution(words, queries):
     answer = []
 
-    for index, query in enumerate(queries):
-        answer.append(0)
-        for word in words:
-            if len(query) != len(word):
-                continue
+    rev_words = []  # words 원소들을 전부 뒤집어서 저장
+    words_length = []  # words 원소들의 길이 저장
+    for word in words:
+        rev_words.append(word[::-1])  # reversed 처리
+        words_length.append(len(word))
 
-            if isMatch(query, word):
-                answer[index] += 1
+    trie = make_trie(words)  # 정방향 trie
+    rev_trie = make_trie(rev_words)  # 역방향 trie
+
+    for query in queries:
+        if query[0] == '?' and query[-1] == '?':  # 모든 글자가 '?'인 경우
+            answer.append(words_length.count(len(query)))
+        elif query[0] == '?':  # '?'로 시작하는 경우 (reversed trie)
+            answer.append(search_trie(rev_trie, query[::-1], len(query)))
+        elif query[-1] == '?':  # '?'로 끝나는 경우 (forward trie)
+            answer.append(search_trie(trie, query, len(query)))
 
     return answer
+
+
+# 주어진 문자열로 trie 구조를 만들어서 반환함.
+def make_trie(words):
+    trie = dict()
+    for word in words:
+        cur = trie
+        for w in word:
+            if w in cur:
+                cur = cur[w]
+                cur['!'].append(len(word))  # '!'를 key로 하여 문자열의 길이 저장
+            else:
+                cur[w] = {}
+                cur = cur[w]
+                cur['!'] = [len(word)]
+    return trie
+
+
+# '?'가 나올 때까지 재귀로 탐색
+#   - '?'를 찾았을 때, 문자열의 길이가 같은 word의 개수만큼 반환.
+def search_trie(trie, query, length):
+    count = 0
+    if query[0] == '?':
+        return trie['!'].count(length)
+    elif query[0] in trie:
+        count += search_trie(trie[query[0]], query[1:], length)  # 재귀
+
+    return count
 
 
 if __name__ == "__main__":
     # [3, 2, 4, 1, 0]
     print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
-
-
-"""
-정확성
-테스트 1 〉	통과 (0.34ms, 10.2MB)
-테스트 2 〉	통과 (0.21ms, 10.2MB)
-테스트 3 〉	통과 (0.25ms, 10.2MB)
-테스트 4 〉	통과 (0.18ms, 10.2MB)
-테스트 5 〉	통과 (0.24ms, 10.2MB)
-테스트 6 〉	통과 (0.21ms, 10.1MB)
-테스트 7 〉	통과 (10.73ms, 10.3MB)
-테스트 8 〉	통과 (12.88ms, 10.2MB)
-테스트 9 〉	통과 (17.60ms, 10.4MB)
-테스트 10 〉	통과 (9.97ms, 10.3MB)
-테스트 11 〉	통과 (8.58ms, 10.2MB)
-테스트 12 〉	통과 (10.72ms, 10.4MB)
-테스트 13 〉	통과 (208.39ms, 10.3MB)
-테스트 14 〉	통과 (180.40ms, 10.6MB)
-테스트 15 〉	통과 (181.00ms, 10.5MB)
-테스트 16 〉	통과 (212.06ms, 10.3MB)
-테스트 17 〉	통과 (188.06ms, 10.5MB)
-테스트 18 〉	통과 (196.14ms, 10.4MB)
-
-효율성
-테스트 1 〉	실패 (시간 초과)
-테스트 2 〉	실패 (시간 초과)
-테스트 3 〉	실패 (시간 초과)
-테스트 4 〉	통과 (632.40ms, 13.3MB)
-테스트 5 〉	통과 (618.14ms, 13.6MB)
-"""
