45bc0c4407a9c74caebdede394c95c22892eaa52
CheolHyun Mun <zooxop@gmail.com>
2022-09-20 15:07:08 +0900

    programmers-swift solved ConvertStringNumber

--- coding/swift/Problem-Solving/Problem-Solving/Programmers/Level1/ConvertStringNumber.swift
+++ coding/swift/Problem-Solving/Problem-Solving/Programmers/Level1/ConvertStringNumber.swift
@@ -1,13 +1,10 @@
-// 1. [main.swift] 파일에서 문제 풀이.
-// 2. 풀이 완료 후, BOJ 또는 Programmers 폴더에 새로운 파일을 생성하여 코드 이관.
-// 3. 2번 작업 완료 후 [main.swift]의 코드를 아래 코드로 변경.
-
-//import Foundation
-//print("Hello, World!")
-
 import Foundation
 
 func solution(_ s:String) -> Int {
+    // 1. 이렇게 풀거나
+    // 2. 딕셔너리로 풀거나
+    // 3. replacingOccurrences(of: "four", with: "4") 로 풀거나
+    
     var answer = ""
     var temp = ""
     
@@ -52,7 +49,7 @@ func solution(_ s:String) -> Int {
     return Int(answer)!
 }
 
-print(solution("one4seveneight"))
-print(solution("23four5six7"))
-print(solution("2three45sixseven"))
-print(solution("123"))
+//print(solution("one4seveneight"))
+//print(solution("23four5six7"))
+//print(solution("2three45sixseven"))
+//print(solution("123"))
