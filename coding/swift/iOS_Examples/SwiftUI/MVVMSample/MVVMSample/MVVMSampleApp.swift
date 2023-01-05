1a5ae28cb00808d2496f90e156dd9326ddd9ec72
CheolHyun Mun <zooxop@gmail.com>
2023-01-06 07:51:36 +0900

    MVVM example in SwiftUI

---
+++ coding/swift/iOS_Examples/SwiftUI/MVVMSample/MVVMSample/MVVMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSampleApp.swift
+//  MVVMSample
+//
+//  Created by 문철현 on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
