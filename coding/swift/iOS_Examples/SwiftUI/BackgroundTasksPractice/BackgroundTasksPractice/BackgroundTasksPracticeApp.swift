86e3a9e7b3efd92388dbdf30452e622a9282c6ce
CheolHyun Mun <zooxop@gmail.com>
2023-03-08 08:29:02 +0900

    UIKit, SwiftUI 폴더 분리

---
+++ coding/swift/iOS_Examples/SwiftUI/BackgroundTasksPractice/BackgroundTasksPractice/BackgroundTasksPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTasksPracticeApp.swift
+//  BackgroundTasksPractice
+//
+//  Created by 문철현 on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTasksPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
