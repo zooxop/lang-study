0097746c600ef34b6fafbd38dbea8fa312a38f12
CheolHyun Mun <zooxop@gmail.com>
2023-04-13 08:01:02 +0900

    add: swift async/await practice example

---
+++ coding/swift/MacOS_Examples/AsyncAwaitTask/AsyncAwaitTask/AsyncAwaitTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitTaskApp.swift
+//  AsyncAwaitTask
+//
+//  Created by 문철현 on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coding/swift/MacOS_Examples/AsyncAwaitTask/AsyncAwaitTask/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  AsyncAwaitTask
+//
+//  Created by 문철현 on 2023/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var result: String = "asdf"
+    @State var test: Bool = false
+    @State var count = 0
+    @State var testCount = 0
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(result)
+            Text(testCount.description)
+            
+            Button {
+                Task {  // 비동기 동작들을 동기로 처리할 수 있게 해준다.
+                    print("start")
+                    test = await test1()
+                    testCount = count
+                    if test == true {
+                        result = "True"
+                    } else {
+                        result = "False"
+                    }
+                    
+                }
+            } label: {
+                Text("Go Go")
+            }
+        }
+        .padding()
+    }
+    
+    func test1() async -> Bool {
+        for _ in 0...1000000 {
+            self.count += 1
+        }
+        print("for문 끝")
+        test.toggle()
+        return test
+    }
+   
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
