5fa31b97d8391503d89d5095f61128a372d8f2a3
Cheolhyun Mun <zooxop@gmail.com>
2023-08-29 09:42:36 +0900

    fix: TCACoordinator 프로젝트 구조 변경

---
+++ coding/swift/iOS_Examples/SwiftUI/TCACoordinatorExample/ConnectorBase/ConnectorBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectorBaseApp.swift
+//  ConnectorBase
+//
+//  Created by 문철현 on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct ConnectorBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
