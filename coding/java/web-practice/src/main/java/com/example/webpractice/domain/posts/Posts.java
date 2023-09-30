f7d267858b0336c75f65b192ae56100e4832f17a
Cheolhyun Mun <zooxop@gmail.com>
2023-09-30 21:01:02 +0900

    feat: BaseTimeEntity 추가

--- coding/java/web-practice/src/main/java/com/example/webpractice/domain/posts/Posts.java
+++ coding/java/web-practice/src/main/java/com/example/webpractice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.webpractice.domain.posts;
 
+import com.example.webpractice.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
