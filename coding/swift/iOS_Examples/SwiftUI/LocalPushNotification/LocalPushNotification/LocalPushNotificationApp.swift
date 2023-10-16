9f5be6fb84dabdd736e74505cc3a3dd64fb72e83
Cheolhyun Mun <zooxop@gmail.com>
2023-10-16 15:59:11 +0900

    add: SwiftUI - Local Push Notification example

---
+++ coding/swift/iOS_Examples/SwiftUI/LocalPushNotification/LocalPushNotification/LocalPushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPushNotificationApp.swift
+//  LocalPushNotification
+//
+//  Created by 문철현 on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct LocalPushNotificationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
