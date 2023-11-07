6ce53b8052d4c1c9f934d703166acf805f1b2e78
Cheolhyun Mun <zooxop@gmail.com>
2023-11-08 00:17:09 +0900

    add: macOS - Launchd Example project init / NLP - 2강

---
+++ coding/swift/MacOS_Examples/LaunchdExample/LaunchdExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchdExample
+//
+//  Created by 문철현 on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coding/swift/MacOS_Examples/LaunchdExample/LaunchdExample/LaunchdExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchdExampleApp.swift
+//  LaunchdExample
+//
+//  Created by 문철현 on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LaunchdExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
