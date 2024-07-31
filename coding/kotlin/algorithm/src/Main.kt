65a8a015f25dfd80259747516c57c8cad005763c
Cheolhyun Mun <zooxop@gmail.com>
2024-07-31 17:47:01 +0900

    [Template] Kotlin 프로그래머스 알고리즘 템플릿 수정

--- coding/kotlin/algorithm/src/Main.kt
+++ coding/kotlin/algorithm/src/Main.kt
@@ -1,13 +1,25 @@
+import com.sun.org.apache.xalan.internal.lib.ExsltMath.power
+import java.util.*
+import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.time.times
 
 fun main() {
     val solution = Solution()
-    val result = solution.solution(2, 91)
+    val result = solution.solution(
+        intArrayOf(3, 7, 2, 5, 4, 6, 1),
+        booleanArrayOf(false, true, true, true, true, false, false)
+    )
+    //println(result.forEach { println(it.contentToString()) })
+    //println(result.forEach { println(it) })
     println(result)
 }
 
 class Solution {
-    fun solution(a: Int, b: Int): Int {
-        return max("$a$b".toInt(), 2*a*b)
+    fun solution(rank: IntArray, attendance: BooleanArray): Int {
+        var answer: Int = 0
+        return answer
     }
 }
