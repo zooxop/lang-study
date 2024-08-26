c15fad2877ab64cb0d30def7e04b31007ebc9b42
Cheolhyun Mun <zooxop@gmail.com>
2024-08-26 13:57:43 +0900

    [Add] 약수 개수 구하기 템플릿

--- coding/kotlin/algorithm/src/archive/Divisors.kt
+++ coding/kotlin/algorithm/src/archive/Divisors.kt
@@ -16,4 +16,19 @@ class Divisors {
         }
         return divisors
     }
+
+    private fun getDivisorsCount(n: Int): Int {
+        if (n == 1) return 1
+        if (n == 2 || n == 3) return 2
+
+        var count = 0
+        val x = sqrt(n.toDouble()).toInt()
+
+        for (i in 1..x) {
+            if (i * i == n) count++
+            else if (n % i == 0) count += 2
+        }
+
+        return count
+    }
 }
