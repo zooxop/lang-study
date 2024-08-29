5948aaad7a9d33a901415dcb86542105614b11b4
Cheolhyun Mun <zooxop@gmail.com>
2024-08-29 21:47:49 +0900

    [Add] 프로그래머스 Lv 1 - 대충 만든 자판

---
+++ coding/kotlin/algorithm/src/programmers/level1/대충 만든 자판.kt
@@ -0,0 +1,39 @@
+package programmers.level1
+
+// 내 풀이
+//private fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
+//    val map = mutableMapOf<Char, Int>()
+//    keymap.forEach { key ->
+//        key.forEachIndexed { i, c ->
+//            if (map.getOrDefault(c, 101) > i) {
+//                map[c] = i + 1
+//            }
+//        }
+//    }
+//
+//    return targets.map { target ->
+//        var count = 0
+//
+//        for (i in 0 until target.length) {
+//            val len = map.getOrDefault(target[i], 0)
+//
+//            if (len == 0) {
+//                count = 0
+//                break
+//            }
+//            count += len
+//        }
+//
+//        if (count == 0) -1 else count
+//    }.toIntArray()
+//}
+
+// (변수 재할당 없이) 영역함수 등으로만 해결한 풀이
+private fun solution(keymap: Array<String>, targets: Array<String>) = keymap
+    .flatMap { it.mapIndexed { idx, char -> char to (idx + 1) } }
+    .groupBy { it.first }
+    .map { it.key to it.value.minOf { v -> v.second } }
+    .toMap()
+    .let { map -> targets.map { it.sumOf { char -> map[char] ?: 10001 } } }
+    .map { if (it >= 10001) -1 else it }
+    .toIntArray()
