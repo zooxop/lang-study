87ff68d831aaae05aec2911b56977c45124480ed
Cheolhyun Mun <zooxop@gmail.com>
2024-09-23 15:51:20 +0900

    [Add] 프로그래머스 Lv 1 - 신고 결과 받기

---
+++ coding/kotlin/algorithm/src/programmers/level1/신고 결과 받기.kt
@@ -0,0 +1,25 @@
+package programmers.level1
+
+import java.util.HashMap
+
+private fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+    val answer = IntArray(id_list.size) { 0 }
+
+    val indexedIdMap = id_list.zip(id_list.indices).toMap()
+    val reportedMap = HashMap<String, MutableSet<String>>()
+
+    report.forEach {
+        val ids = it.split(" ")
+        reportedMap.getOrPut(ids[1]) { mutableSetOf() }.add(ids[0])
+    }
+
+    reportedMap.filter {
+        it.value.size >= k
+    }.forEach { reported ->
+        reported.value.forEach {
+            answer[indexedIdMap[it]!!]++
+        }
+    }
+
+    return answer
+}
