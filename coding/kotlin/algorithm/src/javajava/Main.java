b8ff863759f00159ca3e89fb701a5adc4e426716
Cheolhyun Mun <zooxop@gmail.com>
2024-10-13 21:36:45 +0900

    [Feat] 프로그래머스 Java 문제풀이 환경 설정

--- coding/kotlin/algorithm/src/javajava/Main.java
+++ coding/kotlin/algorithm/src/javajava/Main.java
@@ -1,7 +1,20 @@
 package javajava;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Solution solution = new Solution();
+        int result = solution.solution(10);
+
+        System.out.println(result);
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 == 0)
+            .sum();
     }
 }
