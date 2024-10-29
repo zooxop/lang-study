5ae5ab8d6e144607c4c0423ab2961eb0989d12e3
Cheolhyun Mun <zooxop@gmail.com>
2024-10-29 20:53:59 +0900

    [Add] boj java 기본 템플릿

---
+++ algorithm/src/algorithm/java/boj/Main.java
@@ -0,0 +1,13 @@
+package algorithm.java.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    }
+}
