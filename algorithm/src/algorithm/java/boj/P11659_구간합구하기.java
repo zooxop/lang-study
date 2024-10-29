8eee9745973ea981b4b7f5c30a8289eee8d2c385
Cheolhyun Mun <zooxop@gmail.com>
2024-10-29 20:51:59 +0900

    [Add] boj 11659 구간 합 구하기

---
+++ algorithm/src/algorithm/java/boj/P11659_구간합구하기.java
@@ -0,0 +1,31 @@
+package algorithm.java.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11659_구간합구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        // 구간합 배열 생성
+        long[] sArray = new long[suNo + 1];
+        for (int i = 1; i <= suNo; i++) {
+            sArray[i] = Long.parseLong(stringTokenizer.nextToken()) + sArray[i - 1];
+        }
+
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(sArray[j] - sArray[i-1]);
+        }
+    }
+}
