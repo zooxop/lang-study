412ee7acea7b779cfa0ce74975f0c73832f7087b
Cheolhyun Mun <zooxop@gmail.com>
2024-11-01 15:40:47 +0900

    [Add] boj 1253 좋다

---
+++ algorithm/src/algorithm/java/boj/gold/P1253_좋다.java
@@ -0,0 +1,49 @@
+package algorithm.java.boj.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P1253_좋다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        int count = 0;
+
+        for (int k = 0; k < N; k++) {
+            long find = A[k];
+            int i = 0;
+            int j = N - 1;
+
+            while (i < j) {
+                if (A[i] + A[j] == find) {
+                    if (i != k && j != k) {
+                        count++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else {
+                        j--;
+                    }
+                } else if (A[i] + A[j] < find) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
