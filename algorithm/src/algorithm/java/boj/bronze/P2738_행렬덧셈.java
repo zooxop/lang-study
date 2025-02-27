060c2a3578b2a776ed9e9e9ac02c072850fb1e4e
Cheolhyun Mun <zooxop@gmail.com>
2025-02-28 02:46:09 +0900

    [Add] boj 2738 행렬 덧셈

---
+++ algorithm/src/algorithm/java/boj/bronze/P2738_행렬덧셈.java
@@ -0,0 +1,34 @@
+package algorithm.java.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2738_행렬덧셈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                System.out.print((A[i][j] + temp) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
